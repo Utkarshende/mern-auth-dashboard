3be906099caae11289ad57a779fced6024b8e897
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-01-02 14:20:24 +0530

    Fix: Hardcoded Render URL fallback to resolve ERR_CONNECTION_REFUSED

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
+const renderURL = 'https://mern-auth-dashboard-backend.onrender.com/api';
+
 const API = axios.create({
-  // This will use your Vercel Env Var, or default to Render
-  baseURL: process.env.REACT_APP_API_URL || 'https://mern-auth-dashboard-backend.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || renderURL,
 });
 
-// Attach Token to every request automatically
 API.interceptors.request.use((req) => {
   const profile = localStorage.getItem('userInfo');
   if (profile) {
